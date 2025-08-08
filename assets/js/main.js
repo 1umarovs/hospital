f4bd48bfaa53faf6b2b2d3a4fdf27af8bcf3e0e8
Muhammadyusuf Umarov <umarovmuhammadyusuf17@gmail.com>
2025-08-08 11:39:30 +0500

    add news

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,46 @@ const videoSwiper = new Swiper('.videoSwiper', {
 
 
 
+document.querySelectorAll(".faq-question").forEach(button => {
+  button.addEventListener("click", () => {
+    const faqItem = button.parentElement;
+
+    faqItem.classList.toggle("active");
+  });
+});
+
+
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('data-target');
+    const targetEl = document.getElementById(targetId);
+    if (targetEl) {
+      targetEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Offcanvas ni yopish
+    const offcanvasEl = document.querySelector('.offcanvas.show');
+    if (offcanvasEl) {
+      const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
+      offcanvas.hide();
+    }
+  });
+});
+
+
+
+
+  document.querySelectorAll('.video-thumb').forEach(thumb => {
+    thumb.addEventListener('click', function () {
+      const videoId = this.dataset.videoId;
+      const iframe = document.createElement('iframe');
+      iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`;
+      iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen";
+      iframe.setAttribute('allowfullscreen', '');
+      iframe.classList.add('rounded');
+
+      this.innerHTML = ''; // remove image and icon
+      this.appendChild(iframe);
+    });
+  });
